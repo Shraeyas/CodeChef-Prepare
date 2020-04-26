ac084acbe0df9d39f9e3c9da9101fd64a5ab80b7
Shraeyas <shraeyassingh@gmail.com>
2020-04-26 16:52:02 +0530

    Stacks and Queues

---
+++ Stacks and Queues/STPAR.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+  freopen("in.in", "r", stdin);
+
+  int n;
+
+  while(cin >> n, n)
+  {
+    vector <int> v(n);
+    for(int i = 0 ; i < n ; i++)
+    {
+      cin >> v[i];
+    }
+
+    stack <int> stk;
+    vector <int> ans;
+
+
+  }
+}
